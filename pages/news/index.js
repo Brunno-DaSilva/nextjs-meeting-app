a9cc9dd417aa720d2e67f277fe72da0816bb8b10
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-03-29 17:11:47 -0500

    Construct a link with regular html

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
 //http://localhost:3000/news
 
 function NewsPage() {
-  return <h1>News Page</h1>;
+  return (
+    <div>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <a href="/news/news-item">News Item</a>
+        </li>
+        <li>
+          <a href="/news/news-today">News Today</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NewsPage;
